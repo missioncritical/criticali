02d1248389c54b83989bc0887ef0319aca40823e
Jeff Hunter <jeff@extrabreit.biz>
2010-07-06 20:06:35 -0700

    Rename Vulture to CriticalI in smarty package.

--- smarty/lib/Smarty/Hook/ProjectInstall.php
+++ smarty/lib/Smarty/Hook/ProjectInstall.php
@@ -3,16 +3,16 @@
 /**
  * Custom install tasks for the Smarty package
  */
-class Smarty_Hook_ProjectInstall implements Vulture_Project_InstallHook {
+class Smarty_Hook_ProjectInstall implements CriticalI_Project_InstallHook {
   /**
    * Invoked when the package is installed in a project
    *
-   * @param Vulture_Package_Version $pkg  The package being installed
-   * @param Vulture_Project $project  The project it is being installed into
-   * @param Vulture_Project_InstallOperation $install The installation instance
+   * @param CriticalI_Package_Version $pkg  The package being installed
+   * @param CriticalI_Project $project  The project it is being installed into
+   * @param CriticalI_Project_InstallOperation $install The installation instance
    */
   public function install($pkg, $project, $install) {
-    $projectFolder = ($project->type() == Vulture_Project::INSIDE_PUBLIC) ? 'private/' : '';
+    $projectFolder = ($project->type() == CriticalI_Project::INSIDE_PUBLIC) ? 'private/' : '';
     
     // add the plugins directory
     $install->mkdir("{$projectFolder}helper_plugins");

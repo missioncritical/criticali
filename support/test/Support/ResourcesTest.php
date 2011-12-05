0d06fe05ebf1bd43af32589c1bc12a2f047a0d23
Jeff Hunter <jeff@missioncriticallabs.com>
2011-12-04 16:46:33 -0800

    Increase test coverage for cache resource type

--- support/test/Support/ResourcesTest.php
+++ support/test/Support/ResourcesTest.php
@@ -1,7 +1,26 @@
 <?php
 
+class Support_ResourcesTest_Cache extends Support_Resources_DefaultCacheProvider {
+  public function __construct() {
+    parent::__construct();
+    $this->cacheStore = new Support_ResourcesTest_CacheStore();
+  }
+}
+
+class Support_ResourcesTest_CacheStore extends Support_Resources_DefaultCacheStore {
+}
+
+class Support_ResourcesTest_Support extends Support_Resources {
+  public static function reset() { self::$instance = null; }
+  public static function inst() { return self::instance(); }
+}
+
 class Support_ResourcesTest extends CriticalI_TestCase {
   
+  public function tearDown() {
+    Support_ResourcesTest_Support::reset();
+  }
+
   public function testCache() {
     $cache = Support_Resources::cache();
     
@@ -11,6 +30,30 @@ class Support_ResourcesTest extends CriticalI_TestCase {
     $this->assertTrue(method_exists($cache, 'expire'));
   }
   
+  public function testRegisterCache() {
+    $provider = new Support_ResourcesTest_Cache();
+    
+    Support_Resources::register_cache($provider, 'test');
+    $this->assertTrue(Support_Resources::cache('test') instanceof Support_ResourcesTest_CacheStore);
+    $this->assertFalse(Support_Resources::cache() instanceof Support_ResourcesTest_CacheStore);
+
+    Support_Resources::register_cache($provider, 'test2', true);
+    $this->assertTrue(Support_Resources::cache('test2') instanceof Support_ResourcesTest_CacheStore);
+    $this->assertTrue(Support_Resources::cache() instanceof Support_ResourcesTest_CacheStore);
+  }
+  
+  public function testSetDefaultCache() {
+    $provider = new Support_ResourcesTest_Cache();
+    
+    Support_Resources::register_cache($provider, 'test');
+    $this->assertTrue(Support_Resources::cache('test') instanceof Support_ResourcesTest_CacheStore);
+    $this->assertFalse(Support_Resources::cache() instanceof Support_ResourcesTest_CacheStore);
+
+    Support_Resources::set_default_cache('test');
+    $this->assertTrue(Support_Resources::cache('test') instanceof Support_ResourcesTest_CacheStore);
+    $this->assertTrue(Support_Resources::cache() instanceof Support_ResourcesTest_CacheStore);
+  }
+
 }
 
 ?>

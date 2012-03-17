40837b3fa3709e51c9dfb11bcfefeea2b132365d
Jeff Hunter <jeff@missioncriticallabs.com>
2012-03-16 23:37:05 -0700

    Update configuration for testing smarty block

--- cache/test/config.php
+++ cache/test/config.php
@@ -10,5 +10,7 @@ $APP_CONFIG['cache']['profiles']['memory_profile']['ttl'] = 0;
 $APP_CONFIG['cache']['profiles']['short_file_profile'] = array();
 $APP_CONFIG['cache']['profiles']['short_file_profile']['engine'] = 'file';
 $APP_CONFIG['cache']['profiles']['short_file_profile']['ttl'] = 5;
+$APP_CONFIG['cache']['profiles']['action'] = array();
+$APP_CONFIG['cache']['profiles']['action']['engine'] = 'memory';
 
 ?>
